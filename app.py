f055c8f79eb3857c3c9982c62f6ce2dccf859320
krrish <87867238+krrish-v@users.noreply.github.com>
2022-08-21 14:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,22 +16,17 @@ def check_login(email_, password_):
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-    return redirect(url_for('login'))
-
 @app.route('/bookmarks/username=<username>', methods=['GET', 'POST'])
 
 def index_page(username):
     links = get_db().out_db(username)
-    
+
     if request.method == 'POST':
         query = request.form['query']
         search = ' of '+ query
 
         return render_template('index.html', username=username, links=links, search=search)
- 
+
     return render_template('index.html', username=username, links=links)
 
 @app.route('/bookmark/add/bookmark=<bookmark>,username=<username>')
@@ -58,6 +53,21 @@ def login():
 
     else: return render_template("login.html")
 
+@app.route('/', methods=['GET', 'POST'])
+
+def upload():
+
+    if request.method == 'POST':
+        f = request.files['fil']
+        print(f)
+        if f.filename != '':
+            f.save(f.filename)
+
+            return redirect(url_for('login'))
+        else:
+            redirect(url_for('upload'))
+    
+    return render_template("upload.html")
 
 if __name__=='__main__':
     app.run(debug=True)
